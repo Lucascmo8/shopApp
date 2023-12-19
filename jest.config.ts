e6a723ba74d114422a27eff0ade8d9cb3ba0a805
Lucas Costa <103662624+Lucascmo8@users.noreply.github.com>
2023-12-19 19:09:40 -0300

    Substituindo o React Native CLI pelo Expo

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+// jest.config.js or jest.config.ts
+module.exports = {
+    preset: 'react-native',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    transform: {
+      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+    },
+  };
+  
