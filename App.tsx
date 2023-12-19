c63292f8f116bc5e4438cbcde6e138538307ddb2
Lucas Costa <103662624+Lucascmo8@users.noreply.github.com>
2023-12-19 20:48:36 -0300

    Instalando o Eslint e o Prettier

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Olá mundo!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5589ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,8 @@
 // jest.config.js or jest.config.ts
 module.exports = {
-    preset: 'react-native',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
-    },
-  };
-  
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+};
